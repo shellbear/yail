72d121e97197db1231d8b5a0b69b614e378bd029
shellbear <shellbear@protonmail.com>
2021-05-03 23:03:14 +0900

    Add Spotify LiveView
    Add Play, Pause, Previous, Next buttons
    Show current playback status

--- lib/yail_web/live/page_live.ex
+++ lib/yail_web/live/page_live.ex
@@ -1,39 +1,100 @@
 defmodule YailWeb.PageLive do
   use YailWeb, :live_view
+  require Logger
+
+  @playback_update_interval :timer.seconds(1)
 
   @impl true
-  def mount(_params, _session, socket) do
-    {:ok, assign(socket, query: "", results: %{})}
-  end
-
-  @impl true
-  def handle_event("suggest", %{"q" => query}, socket) do
-    {:noreply, assign(socket, results: search(query), query: query)}
-  end
-
-  @impl true
-  def handle_event("search", %{"q" => query}, socket) do
-    case search(query) do
-      %{^query => vsn} ->
-        {:noreply, redirect(socket, external: "https://hexdocs.pm/#{query}/#{vsn}")}
-
-      _ ->
-        {:noreply,
-         socket
-         |> put_flash(:error, "No dependencies found matching \"#{query}\"")
-         |> assign(results: %{}, query: query)}
-    end
-  end
-
-  defp search(query) do
-    if not YailWeb.Endpoint.config(:code_reloader) do
-      raise "action disabled when not in development"
+  def mount(
+        _params,
+        %{"spotify_access_token" => access_token, "spotify_refresh_token" => refresh_token} =
+          _session,
+        socket
+      ) do
+    if connected?(socket) do
+      Process.send_after(self(), :update, 0)
+      :timer.send_interval(@playback_update_interval, self(), :update)
     end
 
-    for {app, desc, vsn} <- Application.started_applications(),
-        app = to_string(app),
-        String.starts_with?(app, query) and not List.starts_with?(desc, ~c"ERTS"),
-        into: %{},
-        do: {app, vsn}
+    Logger.debug("CONNECTED: #{connected?(socket)}")
+
+    assigns = %{
+      is_playing: false,
+      track_name: "",
+      track_image: "",
+      refresh_token: refresh_token,
+      access_token: access_token
+    }
+
+    {:ok, assign(socket, assigns)}
   end
+
+  @impl true
+  def handle_event("play", _params, socket) do
+    socket =
+      case Spotify.Player.play(get_credentials(socket)) do
+        :ok -> socket = assign(socket, :is_playing, true)
+        _ -> socket
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("pause", _params, socket) do
+    socket =
+      case Spotify.Player.pause(get_credentials(socket)) do
+        :ok -> socket = assign(socket, :is_playing, false)
+        _ -> socket
+      end
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("next", _params, socket) do
+    Spotify.Player.skip_to_next(get_credentials(socket))
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("previous", _params, socket) do
+    Spotify.Player.skip_to_previous(get_credentials(socket))
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info(:update, socket) do
+    Logger.info("Processing...")
+
+    socket =
+      case Spotify.Player.get_current_playback(get_credentials(socket)) do
+        {:ok, playback} ->
+          images = playback.item.album["images"]
+          image = Enum.find(images, &(&1["height"] == 300)) || images[0]
+
+          assign(socket, %{
+            is_playing: playback.is_playing,
+            track_name: playback.item.name,
+            track_image: image["url"]
+          })
+
+        {:error, reason} ->
+          Logger.error("Failed to fetch playback: #{reason}")
+          socket
+
+        _ ->
+          socket
+      end
+
+    {:noreply, socket}
+  end
+
+  def get_credentials(socket),
+    do: %Spotify.Credentials{
+      access_token: socket.assigns.access_token,
+      refresh_token: socket.assigns.refresh_token
+    }
 end
--- lib/yail_web/plugs/auth_plug.ex
+++ lib/yail_web/plugs/auth_plug.ex
@@ -8,9 +8,16 @@ defmodule YailWeb.AuthPlug do
 
   def call(conn, _opts) do
     case fetch_cookies(conn) do
-      %{cookies: %{"spotify_access_token" => _}} ->
+      %{
+        cookies: %{
+          "spotify_access_token" => access_token,
+          "spotify_refresh_token" => refresh_token
+        }
+      } ->
         conn
         |> assign(:is_authenticated, true)
+        |> put_session(:spotify_access_token, access_token)
+        |> put_session(:spotify_refresh_token, refresh_token)
         |> Phoenix.Controller.put_flash(:info, "Authenticated")
 
       _ ->
