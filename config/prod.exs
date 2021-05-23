5ba3bc99b18bc053b2bd34740c8d9f5b0b360f05
shellbear <shellbear@protonmail.com>
2021-05-23 19:48:00 +0900

    Gix an issue with elixir release configs

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,8 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :yail, YailWeb.Endpoint,
-  url: [host: "example.com", port: 80],
+  server: true,
+  code_reloader: false,
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
--- config/release.exs
+++ config/release.exs
@@ -1,6 +1,7 @@
 import Config
 
 secret_key_base = System.get_env("SECRET_KEY_BASE")
+base_url = System.get_env("BASE_URL") || Application.get_env(:yail, :base_url)
 
 if !Application.get_env(:yail, :secret_key_base) do
   case secret_key_base do
@@ -9,4 +10,17 @@ if !Application.get_env(:yail, :secret_key_base) do
   end
 end
 
-config :yail, YailWeb.Endpoint, secret_key_base: secret_key_base
+if !base_url do
+  raise "BASE_URL configuration option is required."
+end
+
+base_url = URI.parse(base_url)
+
+if base_url.scheme not in ["http", "https"] do
+  raise "BASE_URL must start with `http` or `https`. Currently configured as `#{System.get_env("BASE_URL")}`"
+end
+
+config :yail, YailWeb.Endpoint,
+  server: true,
+  url: [host: base_url.host, scheme: base_url.scheme, port: base_url.port],
+  secret_key_base: secret_key_base
